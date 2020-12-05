6948dfdd972071049a3a062d9b13f9213451f434
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2020-12-05 15:23:58 -0600

    problem 5 and default problem foundation

---
+++ default/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AocSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Provide input file name.");
+                return;
+            }
+
+            // TODO
+
+            using (StreamReader fileStream = new StreamReader(new FileInfo(args[0]).OpenRead()))
+            {
+                while (fileStream.Peek() >= 0)
+                {
+                    // TODO
+                }
+            }
+        }
+    }
+}
