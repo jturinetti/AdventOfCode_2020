8c14d976b36b34d8b988e65bc5849c20a35d22d1
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2020-12-05 15:48:21 -0600

    aoc5 part 1 complete

--- 5/Program.cs
+++ 5/Program.cs
@@ -16,15 +16,38 @@ namespace Problem5
                 return;
             }
 
-            // TODO
+            var maximumSeatId = 0;
 
             using (StreamReader fileStream = new StreamReader(new FileInfo(args[0]).OpenRead()))
             {
                 while (fileStream.Peek() >= 0)
                 {
-                    // TODO
+                    var boardingPassEntry = ConvertToBinary(fileStream.ReadLine());
+                    
+                    var row = boardingPassEntry.Substring(0, 7);
+                    var column = boardingPassEntry.Substring(7);
+
+                    var rowByte = Convert.ToByte(row, 2);
+                    Console.WriteLine(rowByte);
+
+                    var columnByte = Convert.ToByte(column, 2);
+                    Console.WriteLine(columnByte);
+                    var seatId = (rowByte * 8) + columnByte;
+                    Console.WriteLine("Seat ID: {0}", seatId);
+
+                    if (seatId > maximumSeatId)
+                    {
+                        maximumSeatId = seatId;
+                    }
                 }
+
+                Console.WriteLine("Maximum Seat ID: {0}", maximumSeatId);
             }
+        }
+
+        private static string ConvertToBinary(string boardingPassEntry)
+        {
+            return boardingPassEntry.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
         }
     }
 }
