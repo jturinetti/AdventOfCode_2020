711fcbb75e3cb54b865a4a5c27c6b10c3b9c4983
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2020-12-06 12:48:24 -0600

    aoc6 part 2 complete

--- 6/Program.cs
+++ 6/Program.cs
@@ -28,7 +28,7 @@ namespace AocSolution
                         var singlePersonResponses = fileStream.ReadLine();                        
                         if (string.IsNullOrWhiteSpace(singlePersonResponses))
                         {
-                            Console.WriteLine("Question Count: {0}", group.NumberOfQuestions);
+                            Console.WriteLine("Question Count: {0}", group.QuestionsAnsweredYesByAtLeastOnePerson);
                             groups.Add(group);
                             break;
                         }
@@ -38,7 +38,8 @@ namespace AocSolution
                 }
 
                 Console.WriteLine("Total Group Count: {0}", groups.Count);
-                Console.WriteLine("Total Question Count: {0}", groups.Sum(g => g.NumberOfQuestions));
+                Console.WriteLine("Total Questions Answered Yes By At Least One Person: {0}", groups.Sum(g => g.QuestionsAnsweredYesByAtLeastOnePerson));
+                Console.WriteLine("Total Questions Answered Yes By Everyone In Group: {0}", groups.Sum(g => g.QuestionsAnsweredYesByEveryone));
             }
         }
     }
@@ -52,6 +53,12 @@ namespace AocSolution
 
         public List<string> QuestionResponses { get; }
         public int NumberOfPeople => QuestionResponses.Count;
-        public int NumberOfQuestions => string.Join("", QuestionResponses).Distinct().Count();
+        public int QuestionsAnsweredYesByAtLeastOnePerson => string.Join("", QuestionResponses)
+                                                                .Distinct()
+                                                                .Count();
+        public int QuestionsAnsweredYesByEveryone => string.Join("", QuestionResponses)
+                                                        .GroupBy(x => x)
+                                                        .Select(x => new { Response = x.Key, ResponseCount = x.Count()})
+                                                        .Count(x => x.ResponseCount == NumberOfPeople);
     }
 }
